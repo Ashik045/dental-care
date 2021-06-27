61a0b400926c7d0bf5f3202e50bcf024830203a9
Your Name <you@example.com>
2021-06-27 17:35:22 +0600

    im trying to creat a dymmy website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import AOS from 'aos';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import DentalCare from './component/Dental_Care/DentalCare';
+import AboutUs from './component/Home_about/AboutUs';
+import MainContact from './component/Home_Contact/MainContact';
+import Mainfooter from './component/Home_Footer/Mainfooter';
+import HomeHeader from './component/Home_Header/HomeHeader';
+import Review from './component/Home_Review/Review';
+import Services from './component/Home_Services/Services';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    AOS.init();
+    return (
+        <div>
+            <HomeHeader />
+            <Services />
+            <DentalCare />
+            <AboutUs />
+            <Review />
+            <MainContact />
+            <Mainfooter />
+        </div>
+    );
 }
 
 export default App;
